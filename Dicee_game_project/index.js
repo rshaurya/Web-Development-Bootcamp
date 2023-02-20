dd04fd47811bd7851922d0c22ea825b7e79a1198
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2023-02-20 13:33:35 +0530

    Modified code by changing methods.

--- Dicee_game_project/index.js
+++ Dicee_game_project/index.js
@@ -1,33 +1,13 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if (randomNumber1 === 1) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice1.png");
-} else if (randomNumber1 === 2) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice2.png");
-} else if (randomNumber1 === 3) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice3.png");
-} else if (randomNumber1 === 4) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice4.png");
-} else if (randomNumber1 === 5) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice5.png");
-} else {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[0].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice6.png");
-}
+var randomDiceImageSource1 = "images/dice" + randomNumber1 + ".png"
 
-if (randomNumber2 === 1) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice1.png");
-} else if (randomNumber2 === 2) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice2.png");
-} else if (randomNumber2 === 3) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice3.png");
-} else if (randomNumber2 === 4) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice4.png");
-} else if (randomNumber2 === 5) {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice5.png");
-} else {
-    document.lastElementChild.lastElementChild.getElementsByClassName("dice")[1].getElementsByTagName("img")[0].setAttribute("src", "/Dicee_game_project/images/dice6.png");
-}
+var randomDiceImageSource2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImageSource1)
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImageSource2)
+
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
