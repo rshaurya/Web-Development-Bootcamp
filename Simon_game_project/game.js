959d112a444b8b7ffdfb3e67953634da3ab8969a
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2023-03-06 21:06:48 +0530

    Working on playing audio of button before user interaction.

--- Simon_game_project/game.js
+++ Simon_game_project/game.js
@@ -1,23 +1,25 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 
 // function nextSequence() {
+
 var randomNumber = Math.floor(Math.random() * 4);
 
 var randomChosenColour = buttonColours[randomNumber];
 
 gamePattern.push(randomChosenColour);
 
-// var idOfElement = randomChosenColour;
+$(idOfElement).fadeIn(100).fadeOut(100).fadeIn(100);
 
-$(randomChosenColour).click(function() {
-    alert(randomChosenColour + " button clicked!")
-})
+var idOfElement = "#" + randomChosenColour;
+
+tmp = "#" + randomChosenColour;
+if (tmp === idOfElement) {
+    var audio = new Audio("sounds/yellow.mp3");
+    audio.play();
+};
 
 // };
 
-nextSequence();
-
-// $("#green").click(function() {
-//     alert("button clicked!");
-// });
+// nextSequence();
